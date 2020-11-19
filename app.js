a6fb42bd15fb6a11d18c2299604f158c594ec7b6
RickySulli <71475646+RickySulli@users.noreply.github.com>
2020-11-19 17:14:33 -0600

    feature/questions

--- app.js
+++ app.js
@@ -30,9 +30,22 @@ const promptUser = () => {
         }  
       },
       {
+        type: 'confirm',
+        name: 'confirmAbout',
+        message: 'Would you like to enter some information about yourself for an "About" section?',
+        default: true
+      },
+      {
           type: 'input',
           name: 'about',
-          message:'Provide some information about yourself:'
+          message:'Provide some information about yourself:',
+          when: ({ confirmAbout }) => {
+            if (confirmAbout) {
+              return true;
+            } else {
+              return false;
+            }
+          }
       }
     ]);
   };
@@ -43,7 +56,7 @@ const promptUser = () => {
     }
       console.log(`
       ===================
-      Add a new Project
+      ADD A NEW PROJECT!!
       ===================
       `);
       return inquirer.prompt([
